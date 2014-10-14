aee2b0996b820364564e53a5c704b942850dc1d8
Emre Surmeli <s.emre.s.8@gmail.com>
2014-10-13 23:05:12 -0700

    js

---
+++ TheShuffler.js
@@ -0,0 +1,32 @@
+	$(function(){
+ 			$.fn.shuffle = function() {
+ 
+	        var allElems = this.get(),
+	            getRandom = function(max) {
+	                return Math.floor(Math.random() * max);
+	            },
+	            shuffled = $.map(allElems, function(){
+	                var random = getRandom(allElems.length),
+	                    randEl = $(allElems[random]).clone(true)[0];
+	                allElems.splice(random, 1);
+	                return randEl;
+	           });
+	 
+	        this.each(function(i){
+	            $(this).replaceWith($(shuffled[i]));
+	        });
+	 		return $(shuffled);
+	 		};
+ 		});
+	        
+		$(function(){
+			$('#enter').click(function(){
+				var input = $('.input').val();
+				$('#only').append('<li>' + input + '</li>');
+				
+				$('button.shuffle').click(function(){
+				$('ul#only li').shuffle();
+				});
+			
+			});	
+		});
