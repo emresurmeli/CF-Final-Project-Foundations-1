9155d32638aec3fccf18999a3920fdbde04be726
Emre Surmeli <s.emre.s.8@gmail.com>
2015-01-18 22:21:46 -0800

    syntax

--- TheShuffler.js
+++ TheShuffler.js
@@ -1,32 +1,32 @@
-	$(function(){
- 			$.fn.shuffle = function() {
- 
-	        var allElems = this.get(),
-	            getRandom = function(max) {
-	                return Math.floor(Math.random() * max);
-	            },
-	            shuffled = $.map(allElems, function(){
-	                var random = getRandom(allElems.length),
-	                    randEl = $(allElems[random]).clone(true)[0];
-	                allElems.splice(random, 1);
-	                return randEl;
-	           });
+$(function(){
+	$.fn.shuffle = function() {
+		var allElems = this.get(),
+	  getRandom = function(max) {
+	    return Math.floor(Math.random() * max);
+	  },
+	
+	shuffled = $.map(allElems, function(){
+		var random = getRandom(allElems.length),
+		randEl = $(allElems[random]).clone(true)[0];
+		allElems.splice(random, 1);
+	 	return randEl;
+	});
 	 
-	        this.each(function(i){
-	            $(this).replaceWith($(shuffled[i]));
-	        });
-	 		return $(shuffled);
-	 		};
- 		});
+	this.each(function(i){
+		$(this).replaceWith($(shuffled[i]));
+	});
+	 	return $(shuffled);
+	 	};
+});
 	        
-		$(function(){
-			$('#enter').click(function(){
-				var input = $('.input').val();
-				$('#only').append('<li>' + input + '</li>');
+$(function(){
+	$('#enter').click(function(){
+		var input = $('.input').val();
+		$('#only').append('<li>' + input + '</li>');
 				
-				$('button.shuffle').click(function(){
-				$('ul#only li').shuffle();
-				});
-			
-			});	
+		$('button.shuffle').click(function(){
+		$('ul#only li').shuffle();
 		});
+			
+	});	
+});
